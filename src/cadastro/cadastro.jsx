502e98ce982499c23807e113bb30556186f66707
william.chenta <william.chenta@gmail.com>
2018-01-21 17:01:11 -0200

    testes

--- src/cadastro/cadastro.jsx
+++ src/cadastro/cadastro.jsx
@@ -9,16 +9,25 @@ export default class Cadastro extends Component {
 
     constructor(props) {
         super(props)
-        this.state = {nome: '', email: ''}
+        this.state = {nome: '', email: '', pessoa: null }
 
         this.handleAdd = this.handleAdd.bind(this)
         this.changeNome = this.changeNome.bind(this)
         this.changeEmail = this.changeEmail.bind(this)
 
         if(this.props.params.id) {
-            console.log('pessoa digitada: ' + this.props.params.id)
+            this.findPessoa(this.props.params.id)
+
+            const pessoa = this.state.pessoa
+
+            console.log('pessoa digitada: ' + pessoa)
         }
         
+    }
+
+    findPessoa(id) {
+        axios.head(URL, { id })
+            .then(resp => console.log('pessoa:' + resp.data)) //this.setState({ pessoa: resp.data })
     }
 
     handleAdd() {
